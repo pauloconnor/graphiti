3ffd2d6539be13ab3bd701a20c68b4040aa874ef
Paul O'Connor <paul.oconnor@swrve.com>
2013-03-08 13:47:08 +0000

    Slightly saner paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 listen 5001 # by default Unicorn listens on port 8080
 worker_processes 2 # this should be >= nr_cpus
-pid "/var/sites/graphiti/shared/pids/unicorn.pid"
-stderr_path "/var/sites/graphiti/shared/log/unicorn.log"
-stdout_path "/var/sites/graphiti/shared/log/unicorn.log"
+pid "/tmp/unicorn_graphiti.pid"
+stderr_path "/tmp/unicorn_graphiti_err.log"
+stdout_path "/tmp/unicorn_graphiti_out.log"
